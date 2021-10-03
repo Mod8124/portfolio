cc80a7ef96efabbbb683e2f876824beb77aa154f
Mod8124 remote <denisenriquebaca@gmail.com>
2021-10-03 13:40:04 -0600

    update design:

--- js.js
+++ js.js
@@ -33,4 +33,26 @@ var observer = new IntersectionObserver(entries => {
   
   observer.observe(document.querySelector('.ps'));
 
+  document.getElementById("denisEmail").setAttribute('onclick', 'location.href = "mailto: denisenriquebaca@gmail.com"');
+  document.getElementById("denisNumber").setAttribute('onclick', 'location.href = "https://wa.me/+50661208412"');
+  document.getElementById("denisGithub").setAttribute('onclick', 'location.href = "https://github.com/Mod8124"');
+  document.getElementById("denisInsta").setAttribute('onclick', 'location.href = "https://www.instagram.com/denismunoz151/"');
+
+  const light = document.getElementsByClassName('light');
+  const redSocial = document.getElementsByClassName('redSocial');
+
+  for(let i = 0; i< redSocial.length; i++) {
+    
+  redSocial[i].addEventListener('mouseover', ()=> {
+    light[i].style.background = '#fff';
+  });
+
+   redSocial[i].addEventListener('mouseleave', ()=> {
+   light[i].style.background = '';
+  });
+ 
+}
+
+
+
 
