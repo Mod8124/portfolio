76e87ca6965b7aef4db9083e812201ada052ea51
denis <denisenriquebaca@gmail.com>
2022-05-11 10:09:36 -0600

    update js

--- js.js
+++ js.js
@@ -1,18 +1,23 @@
 const hambu = document.querySelector('.hambu')
 const containerMobile = document.querySelector('.containMobile');
 
-hambu.addEventListener('click',() => {
- if(hambu.classList.contains('active')) {
-  hambu.classList.remove('active')
-  containerMobile.style.display = 'none'
- } else {
-  hambu.classList.add('active')
-  containerMobile.style.display='flex'
-  setTimeout(()=> {
-      containerMobile.classList.add('active')
-  },100)
- }
-})
+function toggleHambu() {
+
+  if(hambu.classList.contains('active')) {
+
+     hambu.classList.remove('active')
+     containerMobile.style.display = 'none'
+
+   } else {
+     hambu.classList.add('active')
+      containerMobile.style.display='flex'
+     setTimeout(()=> {
+          containerMobile.classList.add('active')
+     },100)
+   }
+}
+
+hambu.addEventListener('click', toggleHambu)
 
 function objectEntry (object, animation) {
   var observer = new IntersectionObserver(entries => {
